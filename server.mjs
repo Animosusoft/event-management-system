129db0c3d27d785fcd82c7087a2526187e8b4b2c
Ultra-Code <mega.alpha100@gmail.com>
2020-08-01 16:11:39 +0000

    Fix R10 error which causes app to continously crash on heroku
    Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
    is fixed by using the Uppercase PORT for port which allows heroku to dynamically allocate port numbers

--- server.mjs
+++ server.mjs
@@ -3,5 +3,7 @@ import serveStatic from "koa-static";
 
 const server = new Koa();
 server.use(serveStatic("./dist"));
-const port = process.env.port || 8000;
-server.listen(port);
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+  console.log("Vue app instance in running on koa at port %s", port);
+});
